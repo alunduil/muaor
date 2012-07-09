20b95f788bb3c8269c468b6e3e698f6167677748
Alex Brandt <alunduil@alunduil.com>
2012-07-09 14:50:18 -0500

    Fixing the license references.

--- lib/mail.rb
+++ lib/mail.rb
@@ -1,6 +1,6 @@
 # Copyright (C) 2012 by Alex Brandt <alunduil@alunduil.com>
 #
-# Expectr is freely distributable under the terms of an MIT-style license.
+# muaor is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 # == Description
