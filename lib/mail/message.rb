1883bc193af1be05da212f99925932cc450d4ec4
Alex Brandt <alunduil@alunduil.com>
2012-07-09 12:20:34 -0500

    Cleaned up insecure code in message.

--- lib/mail/message.rb
+++ lib/mail/message.rb
@@ -11,51 +11,43 @@ module Mail
       @mailbox = mailbox
       @connection = @mailbox.send(:connection)
       @lock = MailboxLock.instance[@connection]
-
-      if kwargs.include? :uid
-        @uid = kwargs.delete(:uid)
-      else
-        @uid = @connection.fetch(msn, "UID").first.attr["UID"]
-      end
-
+      @uid = @connection.fetch(msn, "UID").first.attr["UID"] unless @uid = kwargs.delete(:uid)
+      @data = {}
       kwargs.each do |k,v|
-        method, property = k.split('.').map { |i| i.downcase }
-        instance_eval("@_#{method} ||= {}")
-        instance_eval("@_#{method}[:#{property}] = '#{v}'.split(':', 2).last.strip")
+        method, property = k.split('.').map { |i| i.downcase.to_sym }
+        @data[method] ||= {}
+        @data[method][property] = v.split(':', 2).last.strip
       end
     end
 
     private_class_method :new
 
-    def to_s
+    attr_reader :uid
+
+    def to_s # :nodoc:
       "#{@uid} # #{headers(:from)} -> #{headers(:to)} :: #{headers(:subject)}"
-    end
-
-    def uid
-      @uid ||= uid!
-    end
-
-    def uid! # Just in case ...
-      @uid = @connection.uid_fetch(@uid, ["UID"]).first.attr["UID"]
     end
 
     def headers(*headers)
       raise ArgumentError, "wrong number of arguments (0 for 1 or more)" if headers.empty?
-      @_headers ||= {}
-      headers!(*headers) unless Set.new(headers).subset? Set.new(@_headers.keys)
-      return @_headers[headers.first] if headers.length == 1
-      @_headers.select { |k,v| headers.include? k }
+      @data[:headers] ||= {}
+
+      unknown_headers = headers.to_set - @data[:headers].keys.to_set
+      headers!(*unknown_headers) unless unknown_headers.empty?
+
+      return @data[:headers][headers.first] if headers.length == 1
+      @date[:headers].select { |k,v| headers.include? k }
     end
 
     def headers!(*headers)
       raise ArgumentError, "wrong number of arguments (0 for 1 or more)" if headers.empty?
-      @_headers ||= {}
+      @data[:headers] ||= {}
+
       keys = Hash.new { |h, k| h[k] = "BODY[HEADER.FIELDS (#{k.to_s.upcase})]" }
-      headers.each do |h| 
-        @_headers.merge!(h => @connection.uid_fetch(uid!, keys[h]).first.attr[keys[h]].split(':', 2).last.strip)
-      end
-      return @_headers[headers.first] if headers.length == 1
-      @_headers.select { |k,v| headers.include? k }
+      headers.each { |h| @data[:headers][h] = @connection.uid_fetch(@uid, keys[h]).first.attr[keys[h]].split(':', 2).last.strip }
+
+      return @data[:headers][headers.first] if headers.length == 1
+      @data[:headers].select { |k,v| headers.include? k }
     end
   end
 end
