0ef193e2d8db395d86b75363ff3bb523115c6928
Alex Brandt <alunduil@alunduil.com>
2012-07-13 00:22:14 -0500

    Clarifying example code.

--- examples/mailfilter.rb
+++ examples/mailfilter.rb
@@ -13,50 +13,44 @@ OptionParser.new do |o|
   o.parse!(ARGV)
 end
 
-print "Password: "
+$stdout.print "Password: "
 $stdout.flush
 `stty -echo`
 opts[:password] = gets.chomp
 `stty echo`
-puts ""
+$stdout.puts ""
 
 s = Mail::Server.new(opts[:protocol], opts[:hostname], opts[:username], opts[:password])
 
+puts "Expunging and flushing all changes ..."
+s.mailboxes.each { |b| b.check }
+
+puts "Deleting all messages in all mailboxes with \"ssl\" in the To: header ..."
 s.mailboxes.each do |b|
   deletes = b.messages("headers.to.=" => "ssl")
-  puts "Delete the following:"
-  deletes.each { |d| puts d }
-  #b.batch(:delete, deletes)
+  b.batch(:delete => deletes)
 end
 
+puts "Moving all messages with a mailing list designation (e.g. /\[.*?\]/) into an appropriate mailbox ..."
 s.mailboxes.each do |b|
-  moves = b.messages("headers.subject.~" => "[.*]")
-  mailbox_moves = {}
-  moves.each do |m|
-    name = /\[(.*)\]/.match(m.headers(:subject))[1]
-    mailbox = s.mailboxes("INBOX/Mailing Lists/#{name}") || s.create_mailbox(name)
-    mailbox_moves[mailbox] ||= []
-    mailbox_moves[mailbox] << m
-  end
-
-  mailbox_moves.each do |k,v|
-    puts "Moving the following into #{k}:"
-    v.each { |m| puts m }
-    #k.batch(:move => v)
+  moves = b.messages("headers.subject.~" => /\[.*?\]/)
+  Set.new(moves).classify { |m| m.headers(:subject).match(/\[(.*?)\]/)[1] }.each do |n, m|
+    mailbox = s.mailboxes("INBOX/Mailing Lists/#{n}") || s.create_mailbox("INBOX/Mailing Lists/#{n}")
+    mailbox.batch(:move => m)
   end
 end
 
+puts "Deleting all mailing list messages older than seven days ..."
 s.mailboxes("INBOX/Mailing Lists/%").each do |b|
   deletes = b.messages("headers.date.<" => Date.today - 7)
-  puts "Delete the following from INBOX/Mailing Lists:"
-  deletes.each { |d| puts d }
-  #b.batch(:delete, deletes)
+  b.batch(:delete => deletes)
 end
 
+puts "Deleting all messages in \"Deleted Items\" ..."
 s.mailboxes("Deleted Items").each do |b|
-  puts "Empty Deleted Items"
-  b.batch(:delete => b.messages)
+  b.batch(:delete => :all)
 end
 
-s.mailboxes.each { |b| b.expunge }
+puts "Expunging and flushing all changes ..."
+s.mailboxes.each { |b| b.check }
 
